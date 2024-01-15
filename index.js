a51dc776a59b5a6fe381731aa770a8998c772d15
dymoy <148160802+dymoy@users.noreply.github.com>
2024-01-15 12:42:38 -0800

    Implemented is-valid-css-color npm package to validate inputted color keyword/ hexadecimal. Successfully creates shape from criteria given to inquirer.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* Include packages needed for this application */
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateSVG = require('/lib/generateSVG.js');
-
+const { isValidColorName, isValidRGB } = require('is-valid-css-color');
+const generateSVG = require('./lib/generateSVG');
 
 /** 
  * Create an array of questions for user input including the following sections:
@@ -12,18 +12,18 @@ const generateSVG = require('/lib/generateSVG.js');
  * Shape color (color keyword or hex)
  */ 
 const questions = [
-    {
-        type: 'input',
-        name: 'text',
-        message: 'What text would you like in your logo? Please enter up to 3 characters. ',
-        validate: validateText
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'What color would you like your text? Please enter a color keyword or a hexadeimal number. ',
-        validate: validateColor
-    }, 
+    // {
+    //     type: 'input',
+    //     name: 'text',
+    //     message: 'What text would you like in your logo? Please enter up to 3 characters. ',
+    //     validate: validateText
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'textColor',
+    //     message: 'What color would you like your text? Please enter a color keyword or a hexadeimal number. ',
+    //     validate: validateColor
+    // }, 
     {
         type: 'list',
         name: 'shape',
@@ -43,15 +43,31 @@ const questions = [
     }
 ];
 
-function validateColor(answer) {
-    // if starts with #, validate hex
-    
-    // else, validate color keyword 
+/**
+ * Uses the CSS.supports() method to verify if the browser supports a given CSS feature, or not
+ * @param {JSON} answer - The provided color keyword or hexadecimal provided by the user 
+ * @returns {Boolean} - Returns true if color keyword or hex value is supported by CSS.
+ */
+function validateColor(answer) {    
+    var isValid = false;
+
+    if (answer[0] === '#') {
+        isValid = isValidRGB(answer);
+    } else {
+        isValid = isValidColorName(answer);
+    }
+
+    if (isValid) {
+        return true;
+    } else {
+        console.log('\x1B[31m Please enter a valid color keyword or hexidecimal number. \x1B[31');
+        return false;
+    }
 }
 
 /**
  * Validate that the text entered is 3 characters or fewer.
- * @param {String} answer - text entered by the user 
+ * @param {String} answer - Text entered by the user 
  * @returns {Boolean} - Returns false if over 3 characters. 
  */
 function validateText(answer) {
@@ -63,14 +79,16 @@ function validateText(answer) {
 }
 
 /**
- * 
+ * Write contents into a file using npm fs package.
+ * @param {String} fileName - the file name to write to. 
+ * @param {String} content - the content to write into the file. 
+ * If Error is encountered, it is logged into the console. If not, a success message is printed to the command line. 
  */
 function writeToFile(fileName, content) {
     fs.writeFile(`./examples/${fileName}`, content, (err) => {
-        err ? console.log(err) : console.log(`Success! Your new SVG file was wrriten to: ./examples/${fileName}`);
+        err ? console.log(err) : console.log(`Success! Generated logo.svg!`);
     });
 }
-
 
 /* Create a function to initialize app */
 function init() {
@@ -79,6 +97,7 @@ function init() {
     // Prompt the user with the questions and then write data into 'README.md'
     inquirer.prompt(questions)
     .then((data) => {
+        console.log(data);
         // Call the writeToFile function to write the content into the output file
         const fileName = 'logo.svg';
         const content = generateSVG(data);
